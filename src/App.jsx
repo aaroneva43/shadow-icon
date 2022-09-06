e1e3a53a61510911e74dcf08ad5f5a442cccd7f2
pengxie <pengxie@fortinet.com>
2022-09-06 14:54:57 -0700

    support svg import

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { ReactComponent as Logo } from '../public/vite.svg'
+import { ReactComponent as Face } from "@material-design-icons/svg/filled/face.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Logo />
+      <Face style={{width: 100, height: 100, background: 'pink'}}/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
